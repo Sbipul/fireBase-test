2aaae495387dc4ec2399b393420967c7d78b7d6c
Bipul Chandro Roy <86865606+Sbipul@users.noreply.github.com>
2022-11-29 12:50:18 +0600

    Update FireBase.config.js

--- src/FireBase/FireBase.config.js
+++ src/FireBase/FireBase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD5d6XsVvR8S5_y_FCWF6m72KpmdS6X07k",
-    authDomain: "test-firebase-92410.firebaseapp.com",
-    projectId: "test-firebase-92410",
-    storageBucket: "test-firebase-92410.appspot.com",
-    messagingSenderId: "152296171064",
-    appId: "1:152296171064:web:c3e64bcbd20a1cbe4ae8cd"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   export default firebaseConfig
